bc1cbf963a9d6a13209a509458b23abb05b02938
spiddy <dimitris.kapanidis@harbur.io>
2020-09-16 09:02:51 +0200

    fix dropdown

--- components/forms/TrialForm.tsx
+++ components/forms/TrialForm.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { encode } from "querystring";
 import { useEffect, useState } from "react";
 import { Divider, FormButton, FormCheckbox, FormDropdown, FormInput, Header, Input, Segment } from "semantic-ui-react";
 import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
@@ -22,12 +23,24 @@ export default function TrialForm() {
     updateEnabled(terms && expectedUsers !== undefined)
   }, [terms, gdpr, expectedUsers])
 
+  const handleSubmit = (e: any) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", terms, gdpr, expectedUsers })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
   return (
     <Segment color="blue" style={{ textAlign: "left", paddingLeft: "30px", paddingRight: "30px", paddingTop: "30px" }}>
       <Header>Get Started with Kubernetic Enterprise in minutes</Header>
       <HeaderSubHeader>After signing up, we will send you a trial license which will be active for 14 days with seats for 3 users. Once you get your license you can follow the installation process in <Link href="https://docs.kubernetic.com/#/installation/enterprise"><a>our guide</a></Link>.</HeaderSubHeader>
       <Divider hidden />
-      <form className="ui form" name="enterprise-trial" method="POST" aria-disabled={true} data-netlify="true">
+      <form onSubmit={handleSubmit} className="ui form" name="enterprise-trial" method="POST" aria-disabled={true} data-netlify="true">
         <Header size="tiny">Trial Details</Header>
         <FormInput label="Your Name" required type="text" name="name" id="name" />
         <FormInput label="Company Email" required type="email" name="email" id="email" />
@@ -36,7 +49,16 @@ export default function TrialForm() {
         <FormInput label="Country" required type="text" name="country" id="country" />
         <FormDropdown selection placeholder="" options={expectedUsersOptions} value={expectedUsers} onChange={(e, { value }) => updateExpectedUsers(value as number)} label="Expected Users" required name="expected-users" id="expected-users" />
         <FormInput label="Phone" name="phone" id="phone" />
-        <Divider hidden/>
+        <Divider hidden />
+        <select className="ui selection dropdown" name="flooring-type[]" required={true}>
+          <option className="item" value="" disabled={true} selected={true}>Choose Flooring type</option>
+          <option value="Laminate flooring">Laminate flooring</option>
+          <option value="Hardwood">Hardwood</option>
+          <option value="SPC">SPC</option>
+          <option value="LVT">LVT</option>
+          <option value="Skirting">Skirting</option>
+        </select>
+
         <FormCheckbox checked={gdpr} onChange={() => updateGDPR(!gdpr)} label={<label>I agree to receive Harbur marketing communications via email. I can always update my preferences later.</label>} name="gdpr" id="gdpr" />
         <FormCheckbox required checked={terms} onChange={() => updateTerms(!terms)} label={<label>I agree to the Harbur <Link href="https://harbur.io/privacy/index.html"><a>Terms of Use & Privacy Policy</a></Link>.</label>} name="terms" id="terms" />
         <Input type="hidden" name="form-name" value="enterprise-trial" />
